ae4ea890b45a9cb57ed272d8e35a5002fdfc94af
Rob Tumarkin <r.tumarkin@unsw.edu.au>
2021-05-14 17:04:14 +1000

    Mouse key speed setting configuration.

--- keyboards/preonic/keymaps/tumarkin/config.h
+++ keyboards/preonic/keymaps/tumarkin/config.h
@@ -80,15 +80,15 @@
 // Delay between pressing a movement key and cursor movement
 #define MOUSEKEY_DELAY 8
 // Time between cursor movements in milliseconds
-#define MOUSEKEY_INTERVAL 12
+#define MOUSEKEY_INTERVAL 8
 // Step size for accelerating from initial to base speed
 #define MOUSEKEY_MOVE_DELTA 1
 // Maximum cursor speed at which acceleration stops
-#define MOUSEKEY_MAX_SPEED 40
+#define MOUSEKEY_MAX_SPEED 22
 // Time until maximum cursor speed is reached
 #define MOUSEKEY_TIME_TO_MAX 200
 // Initial speed of the cursor in pixel per second
-#define MOUSEKEY_INITIAL_SPEED 2
+#define MOUSEKEY_INITIAL_SPEED 1
 // // Maximum cursor speed at which acceleration stops
 // #define MOUSEKEY_BASE_SPEED 60
 // // Decelerated cursor speed
